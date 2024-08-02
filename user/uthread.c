da2849b2290d8b9b306cc6df715f50ecc755b2fa
POTUS47 <2923289295@qq.com>
2024-08-02 05:02:26 -0400

    Uthread: switching between threads OK

--- user/uthread.c
+++ user/uthread.c
@@ -10,11 +10,32 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct utcontext
+{
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct utcontext utcontext;
 };
+
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
@@ -32,22 +53,25 @@ thread_init(void)
 void 
 thread_schedule(void)
 {
+
   struct thread *t, *next_thread;
 
   /* Find another runnable thread. */
   next_thread = 0;
-  t = current_thread + 1;
+  t = current_thread + 1; // 从当前线程的下一个位置开始查找可运行线程。
   for(int i = 0; i < MAX_THREAD; i++){
     if(t >= all_thread + MAX_THREAD)
       t = all_thread;
-    if(t->state == RUNNABLE) {
+    if(t->state == RUNNABLE)
+    { // 找到一个状态为 RUNNABLE 的线程，则将其赋给 next_thread，并跳出循环。
       next_thread = t;
       break;
     }
     t = t + 1;
   }
 
-  if (next_thread == 0) {
+  if (next_thread == 0)
+  { // 如果没有找到可运行的线程，则输出错误信息并终止程序。
     printf("thread_schedule: no runnable threads\n");
     exit(-1);
   }
@@ -56,11 +80,13 @@ thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
-    /* YOUR CODE HERE
+    /* YOUR CODE HERE 
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
-  } else
+    thread_switch((uint64)&t->utcontext, (uint64)&next_thread->utcontext);
+  }
+  else // 没有进行线程切换，将 next_thread 设置为 0，表示没有新的可运行线程。
     next_thread = 0;
 }
 
@@ -72,14 +98,18 @@ thread_create(void (*func)())
   for (t = all_thread; t < all_thread + MAX_THREAD; t++) {
     if (t->state == FREE) break;
   }
-  t->state = RUNNABLE;
+  t->state = RUNNABLE; // 找到一个空闲线程后，将该线程的状态设置为 RUNNABLE。
+                       // 这意味着这个线程现在是可运行状态，可以被调度器选中执行。
   // YOUR CODE HERE
+  t->utcontext.ra = (uint64)func;
+  t->utcontext.sp = (uint64)&t->stack[STACK_SIZE];
 }
 
 void 
 thread_yield(void)
 {
-  current_thread->state = RUNNABLE;
+
+  current_thread->state = RUNNABLE;//现在在运行的线程设为runnable
   thread_schedule();
 }
 
@@ -152,7 +182,7 @@ main(int argc, char *argv[])
   a_started = b_started = c_started = 0;
   a_n = b_n = c_n = 0;
   thread_init();
-  thread_create(thread_a);
+  thread_create(thread_a);//创建一个新的线程并执行thread_a函数
   thread_create(thread_b);
   thread_create(thread_c);
   current_thread->state = FREE;
